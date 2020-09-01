c7d9bb988c5154006c799f96ee3051aff609442b
liuyan <15076029238@163.com>
2020-09-01 16:21:32 +0800

    增加标准法规库模块

---
+++ src/pages/regulatoryRepository/index.vue
@@ -0,0 +1,16 @@
+<!-- 标准法规库 -->
+<template>
+  <div class="regulatory-repository">
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  name: 'RegulatoryRepository'
+}
+</script>
+<style lang="less" scoped>
+  .regulatory-repository{
+    height: 100%;
+  }
+</style>
