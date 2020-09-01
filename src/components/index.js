a7dcdb457ea5b342eec0f81e5e1d9867747760df
liuyan <15076029238@163.com>
2020-09-01 09:53:01 +0800

    增加table分页组件

--- src/components/index.js
+++ src/components/index.js
@@ -6,12 +6,14 @@ import Loading from './loading' // loading组件
 import NavMenu from './navMenu' // 左侧菜单栏组件
 import ComHeader from './comHeader' // header头部组件 面包屑、退出
 import LawsTree from './lawsTree' // tree组件
+import Pagination from './pagination' // table表格分页组件
 
 const install = function (Vue) {
   Vue.component('loading', Loading)
   Vue.component('navMenu', NavMenu)
   Vue.component('comHeader', ComHeader)
   Vue.component('lawsTree', LawsTree)
+  Vue.component('pagination', Pagination)
 }
 
 export default {
